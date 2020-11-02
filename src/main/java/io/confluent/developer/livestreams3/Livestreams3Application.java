d83b37983abab4a184ed494dfee4e9353b853b90
Sagun <sshrestha.me@gmail.com>
2020-11-02 17:26:36 -0500

    git init

---
+++ src/main/java/io/confluent/developer/livestreams3/Livestreams3Application.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.livestreams3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Livestreams3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Livestreams3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/confluent/developer/livestreams3/Livestreams3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.confluent.developer.livestreams3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Livestreams3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
